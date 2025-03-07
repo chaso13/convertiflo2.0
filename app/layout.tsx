286eecf0dbda6b752618f757d3884dbe038c2d1a
chaso13 <chaso13@yahoo.com>
2025-03-07 00:51:24 -0500

    convertiflo first commit

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/navbar";
+import { Toaster } from "@/components/ui/toaster";
+import GA from "@/components/GA";
+import { ThemeProvider } from "@/components/theme-provider";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Convertiflo - Free Unlimited File Converter",
+    description: `Unleash your creativity with Convertiflo – the ultimate online tool for
+  unlimited and free multimedia conversion. Transform images, audio, and
+  videos effortlessly, without restrictions. Start converting now and
+  elevate your content like never before!`,
+    creator: "Chase Heavner",
+    keywords: "image converter, video converter, audio converter, unlimited image converter, unlimited video converter",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <ThemeProvider attribute="class" defaultTheme="system" enableSystem themes={["light", "dark"]}>
+                    <Navbar />
+                    <Toaster />
+                    <div className="pt-32 min-h-screen lg:pt-36 2xl:pt-44 container max-w-4xl lg:max-w-6xl 2xl:max-w-7xl">
+                        {children}
+                    </div>
+                </ThemeProvider>
+            </body>
+        </html>
+    );
+}
